23aae8f05dcf87940f8b22e05d9384187ce026c6
Matsuha1008 <72287751+Matsuha1008@users.noreply.github.com>
2021-12-27 20:41:12 +0800

    -

--- CNB_LineBot/urls.py
+++ CNB_LineBot/urls.py
@@ -14,18 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from django.conf.urls import url
-from django.urls.conf import include, include
-
-from django.views.static import serve
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from django.contrib import staticfiles
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('CNB_Bot/', include('CNB_Bot.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
