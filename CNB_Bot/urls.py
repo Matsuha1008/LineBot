2149c64e0e177c97feab30fb4c2d7505b80a4ee4
Matsuha1008 <72287751+Matsuha1008@users.noreply.github.com>
2021-12-27 20:44:50 +0800

    -

--- CNB_Bot/urls.py
+++ CNB_Bot/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    path('callback', views.callback),  
+    path('callback', views.callback),
+    path('confirm', views.confirm),
+    url(r'^confirm', views.confirm),
 ]
